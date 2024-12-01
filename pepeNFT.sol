614c4c9525c3c0962d527b8a60881f0a2249fbea
luke90329 <32509228+luke90329@users.noreply.github.com>
2024-12-02 03:44:33 +0800

    init

---
+++ pepeNFT.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract pepeNFT is ERC721URIStorage {
+    uint256 private _currentTokenId = 1;
+    string private baseURI = "https://bafybeidt5mxd77pknt5l4tzxnnsz4oru5wrauxhyndmotxrvrf4wmrorzm.ipfs.dweb.link/";
+    mapping(address => bool) private minted;
+
+    constructor() ERC721("Pepe", "PEPE") {}
+
+    function mintPepe() public returns (uint256){
+        require(_currentTokenId <= 20, "Exceed mint limit");
+        require(!minted[msg.sender], "Already have a pepe");
+        uint256 tokenId = _currentTokenId++;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, string.concat(baseURI, Strings.toString(tokenId), ".json"));
+        minted[msg.sender] = true;
+
+        return tokenId;
+    }
+}
